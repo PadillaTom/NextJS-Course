8bc3ca644d43086037b7567c322f2cba50e82a95
Tomas Padilla <padillatomasagustin@gmail.com>
2024-06-17 11:04:28 +0200

    ts-react - ContextAPI and type definitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
-import Component from "./starter/05-challenge";
+import Component from "./starter/06-context";
 
 function App() {
 	return (
 		<main>
-			{/* <Component type="advanced" name="susan" email="asd@asd.com"></Component> */}
-			{/* <Component type="basic" name="susan" ></Component> */}
-			<Component type="basic" name="susan"></Component>
-			<Component type="advanced" name="susan" email="hello@hello.com"></Component>
+			<Component></Component>
 		</main>
 	);
 }
--- src/starter/06-context/index.tsx
+++ src/starter/06-context/index.tsx
@@ -1,9 +1,36 @@
-function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Context API</h2>
-    </div>
-  );
+import { useTheme, ThemeProvider } from "./context";
+
+function ParentComponent() {
+	return (
+		<ThemeProvider>
+			<Component></Component>
+		</ThemeProvider>
+	);
 }
-export default Component;
+
+function Component() {
+	const context = useTheme();
+	console.log(context);
+
+	return (
+		<div>
+			<h2>React & Typescript</h2>
+			<h2>Context API</h2>
+
+			<button
+				type="button"
+				className="btn btn-center"
+				onClick={() => {
+					if (context.theme === "dark") {
+						context.setTheme("system");
+						return;
+					}
+					context.setTheme("dark");
+				}}
+			>
+				Toggle Theme
+			</button>
+		</div>
+	);
+}
+export default ParentComponent;
