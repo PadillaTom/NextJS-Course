328ba248f659c9adc4685361e3a64c5a8527f979
Tomas Padilla <padillatomasagustin@gmail.com>
2024-06-17 15:35:25 +0200

    ts-react - Fetching data basic structure

--- src/starter/08-fetch-data/index.tsx
+++ src/starter/08-fetch-data/index.tsx
@@ -1,9 +1,42 @@
+import { useState, useEffect } from "react";
+const url = "https://www.course-api.com/react-tours-project";
+
 function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Fetch Data</h2>
-    </div>
-  );
+	// Tours
+	const [isLoading, setIsLoading] = useState(false);
+	const [isError, setIsError] = useState<string | null>(null);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			setIsLoading(true);
+			try {
+				const resposne = await fetch(url);
+				if (!resposne.ok) {
+					throw new Error("no response");
+				}
+				const rawData = await resposne.json();
+				console.log(rawData);
+			} catch (error) {
+				const message = error instanceof Error ? error.message : "Error....";
+				setIsError(message);
+			} finally {
+				setIsLoading(false);
+			}
+		};
+		fetchData();
+	}, []);
+
+	if (isLoading) {
+		return <h3>Loading...</h3>;
+	}
+	if (isError) {
+		return <h3>Error: {isError}</h3>;
+	}
+	return (
+		<div>
+			<h2>React & Typescript</h2>
+			<h2>Fetch Data</h2>
+		</div>
+	);
 }
 export default Component;
