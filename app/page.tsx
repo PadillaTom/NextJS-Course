b94aa81d4f492b1d63a99b3281cbb7079424adc6
Tomas Padilla <padillatomasagustin@gmail.com>
2024-06-17 16:53:20 +0200

    nextjs - Basic Setup with Ts, Tailwind

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import React from "react";
+
+function Homepage() {
+	return (
+		<div>
+			<h1 className="text-7xl">HomePage</h1>
+			<Link href={"/about"}>About</Link>
+			<Link href={"/info"}>Info</Link>
+		</div>
+	);
+}
+
+export default Homepage;
