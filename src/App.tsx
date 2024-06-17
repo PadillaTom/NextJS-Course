1fe6b066b5ac50229d1b7ba3d3ae3a261a8a9109
Tomas Padilla <padillatomasagustin@gmail.com>
2024-06-17 10:17:32 +0200

    ts-react - Challenge multiple prop types

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import Component from "./starter/04-events";
+import Component from "./starter/05-challenge";
 
 function App() {
 	return (
 		<main>
-			<Component></Component>
+			{/* <Component type="advanced" name="susan" email="asd@asd.com"></Component> */}
+			{/* <Component type="basic" name="susan" ></Component> */}
+			<Component type="basic" name="susan"></Component>
+			<Component type="advanced" name="susan" email="hello@hello.com"></Component>
 		</main>
 	);
 }
--- src/starter/05-challenge/index.tsx
+++ src/starter/05-challenge/index.tsx
@@ -1,9 +1,39 @@
-function Component() {
-  return (
-    <div>
-      <h2>React & Typescript</h2>
-      <h2>Challenge</h2>
-    </div>
-  );
+// Challenge is:
+// Create a type that has dynamic properties based on a given property.
+// Example: ProfileCard has TYPE property. Depending on which one it is, I will have EMAIL or not.
+// <Component type="advanced" name="susan" email="asd@asd.com"></Component>
+// <Component type="basic" name="susan" ></Component>
+
+// SOLUTION: HAVE TWO DIFFERENT TYPES
+
+type BasicProfileCardProps = {
+	type: "basic";
+	name: string;
+};
+type AdvancedProfileCardProps = {
+	type: "advanced";
+	name: string;
+	email: string;
+};
+
+type ProfileCardProps = BasicProfileCardProps | AdvancedProfileCardProps;
+
+function Component(props: ProfileCardProps) {
+	const { type, name } = props;
+
+	if (type === "basic") {
+		return (
+			<article className="alert alert-success">
+				<h2>User: {name}</h2>
+			</article>
+		);
+	}
+
+	return (
+		<article className="alert alert-danger">
+			<h2>User: {name}</h2>
+			<h2>Email: {props.email}</h2>
+		</article>
+	);
 }
 export default Component;
