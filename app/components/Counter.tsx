a38d6347b8a335c2e340bddd1d3fce28b3518aca
Tomas Padilla <padillatomasagustin@gmail.com>
2024-06-18 17:59:45 +0200

    nextjs - Server Components vs Client Components

---
+++ app/components/Counter.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { useState } from "react";
+
+const Counter = () => {
+	const [count, setCount] = useState(0);
+
+	return (
+		<div className="flex flex-col items-center w-[100px]">
+			<p className="text-5xl font-bold">{count}</p>
+			<button className="bg-blue-500 rounder text-white px-4 py-2 mt-4">Increment</button>
+		</div>
+	);
+};
+
+export default Counter;
