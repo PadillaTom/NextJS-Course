dc6d730c1a6315a18e5cda49ae827efe84ce2854
Tomas Padilla <padillatomasagustin@gmail.com>
2024-06-18 18:19:49 +0200

    nextjs - Nested Layouts and Dynamic Pages

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -1,6 +1,7 @@
+import Link from "next/link";
 import React from "react";
 
-const url = "https://www.course-api.com/react-tours-projects";
+const url = "https://www.course-api.com/react-tours-project";
 
 type Tour = {
 	id: string;
@@ -23,7 +24,11 @@ async function ToursPage() {
 		<section>
 			<h1 className="text-3xl mb-4">Tours</h1>
 			{data.map((i) => {
-				return <h2 key={i.id}>{i.name}</h2>;
+				return (
+					<Link key={i.id} href={`/tours/${i.id}`} className="hover:text-blue-500">
+						<h2>{i.name}</h2>
+					</Link>
+				);
 			})}
 		</section>
 	);
