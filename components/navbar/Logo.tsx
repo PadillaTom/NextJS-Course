6c4b38f038426dba54b38a82d66af146ae9fe875
Tomas Padilla <padillatomasagustin@gmail.com>
2024-06-19 08:23:13 +0200

    nextstore - Logo using react-icons and Shadcn button asChild

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import Link from "next/link";
+import { VscCode } from "react-icons/vsc";
+
+import { Button } from "../ui/button";
 
 function Logo() {
-	return <div>Logo</div>;
+	return (
+		<Button size={"icon"} asChild>
+			<Link href="/">
+				<VscCode></VscCode>
+			</Link>
+		</Button>
+	);
 }
 
 export default Logo;
