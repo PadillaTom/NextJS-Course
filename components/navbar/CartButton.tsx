20b0379c27bbc4aba14504fafd6e64f00192ba52
Tomas Padilla <padillatomasagustin@gmail.com>
2024-06-19 08:27:55 +0200

    nextstore - Cart and Navsearch

--- components/navbar/CartButton.tsx
+++ components/navbar/CartButton.tsx
@@ -1,7 +1,21 @@
 import React from "react";
+import Link from "next/link";
+import { LuShoppingCart } from "react-icons/lu";
+import { Button } from "../ui/button";
 
 function CartButton() {
-	return <div>CartButton</div>;
+	// Temporary:
+	const numItemsInCart = 9;
+	return (
+		<Button asChild variant={"outline"} size={"icon"} className="flex justify-center items-center relative">
+			<Link href={"/cart"}>
+				<LuShoppingCart></LuShoppingCart>
+				<span className="absolute -top-3 -right-3 bg-primary text-white rounded-full h-6 w-6 flex items-center justify-center text-xs">
+					{numItemsInCart}
+				</span>
+			</Link>
+		</Button>
+	);
 }
 
 export default CartButton;
